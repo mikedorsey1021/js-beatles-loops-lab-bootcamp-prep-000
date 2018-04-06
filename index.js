ba07659737380569afd3fe9cc7350516b1439ad8
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-06 10:54:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay (array1, array2){
+  var arr = []; 
+  for (i = 0; i < array1.length; i++){
+    arr.push("`${array1[i]}`" + " plays " + "`${array2}`"); 
+  }
+  return arr 
+}
